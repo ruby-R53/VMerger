678122daceff1567cc0a429bcf7b223624696f96
ruby-R53 <enkiferreiracosta867@gmail.com>
2025-09-01 23:48:37 -0300

    support for concat demuxer, untested
    plus better error handling and fancier text output
    i've actually tried the muxer one out, but got a glitchy video as a result
    what i didn't test however, was the script in this form to render the same video, i've been writing this new code to an backup file this whole time

--- vmerger.sh
+++ vmerger.sh
@@ -5,9 +5,19 @@
 # together using FFmpeg.
 # Written by ruby R53 on August 2025.
 
-# increase safety by aborting on unset
-# variable access
-set -u
+error() {
+	tput setaf 1; tput bold
+	echo -n "ERROR"
+	tput setaf 7
+	echo -n ": "
+	tput sgr0
+	echo $1
+
+	exit 1
+}
+
+[[ -z $1 ]] &&
+	error "a file must be supplied!"
 
 # make ffmpeg a lil' quiet so that we don't
 # pollute our screen with so much info
@@ -21,18 +31,20 @@ THREADS="$(nproc)"
 # change this to your liking
 TEXT_PARAMS=("drawtext='fontfile=Noto Sans\\:style=Bold':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=h-th-10")
 # miscellaneous render arguments
-RENDER_ARGS=(-c:v libx264 -c:a copy -crf 0 -qp 0 -preset ultrafast -color_range 1 -colorspace bt709 -color_trc bt709 -color_primaries bt709 -movflags faststart)
+RENDER_ARGS=(-c:v libx264 -c:a copy -crf 0 -qp 0 \
+	-preset ultrafast -color_range 1 -colorspace bt709 \
+	-color_trc bt709 -color_primaries bt709 -movflags faststart)
 # this is where our actual text parameters
 # will be placed, in case we wanna use
 # another video filter option
 VIDEO_FLTRS=(scale=out_color_matrix=bt709,"${TEXT_PARAMS[@]}")
 
+# check if any captions are present and apply them
 # get how many files we have to work on
 ENTRIES=("$(sed '$ d' $1 | awk -F"=" '{ print $1 }' | tr '\n' ' ')")
 # and our output file name
-OUTFILE=$(awk 'END { print }' $1)
+OUTFILE=$(awk -e 'END { print }' -F"=" -e '{ print $1 }' $1)
 
-# check if any captions are present and apply them
 for FILE in ${ENTRIES[@]}; do
 	# get label for current video
 	LABEL="$(grep "${FILE}" $1 | awk -F"=" '{ print $2 }')"
@@ -48,8 +60,11 @@ for FILE in ${ENTRIES[@]}; do
 
 	# show the user which file we're at, in case ffmpeg doesn't
 	# already do that
-	[[ ${LOGLVL} = "warning" ]] || [[ ${LOGLVL} = "quiet" ]] &&
-		echo "${FILE}"
+	if [[ ${LOGLVL} = "warning" ]] || [[ ${LOGLVL} = "quiet" ]]; then
+		tput bold
+		echo "Labeling ${FILE}..."
+		tput sgr0
+	fi
 
 	# then, actually apply the label to the current video
 	ffmpeg -v "${LOGLVL}" -threads "${THREADS}" -i "${FILE}"\
@@ -61,31 +76,60 @@ for FILE in ${ENTRIES[@]}; do
 	[[ $? != '0' ]] && rmdir "vmerger/" && exit 1
 done
 
-# feed files to ffmpeg's input
-for FILE in ${ENTRIES[@]}; do
-	# check if the user wants to merge labeled
-	# or unlabeled videos
-	if [[ -d "$(pwd)/vmerger" ]] &&
-		# also make sure we're pointing to a
-		# labeled video file
-		[[ ! -z "$(grep "$FILE" $1 | awk -F"=" '{ print $2 }')" ]]
-	then
-		FILES+=(-i "vmerger/"${FILE}"")
-	else
-		FILES+=(-i "${FILE}")
-	fi
-done
+MERGETYPE="$(awk -e 'END { print }' -F"=" -e '{ print $2 }' $1)"
+case $MERGETYPE in
+	"filter" | "f")
+		# feed files to ffmpeg's input
+		for FILE in ${ENTRIES[@]}; do
+			# check if the user wants to merge labeled
+			# or unlabeled videos
+			if [[ -d "$(pwd)/vmerger" ]] &&
+				# also make sure we're pointing to a
+				# labeled video file
+				[[ ! -z "$(grep "$FILE" $1 | awk -F"=" '{ print $2 }')" ]]
+			then
+				FILES+=(-i "vmerger/"${FILE}"")
+			else
+				FILES+=(-i "${FILE}")
+			fi
+		done
 
-# tell ffmpeg about the streams we have
-NSTREAMS=$(sed '$ d' $1 | wc -l)
-for (( i = 0; i <= $(( NSTREAMS - 1 )); i++ )); do
-	STREAMS+="[${i}:v][${i}:a]"
-done
+		# tell ffmpeg about the streams we have
+		NSTREAMS=$(sed '$ d' $1 | wc -l)
+		for (( i = 0; i <= $(( NSTREAMS - 1 )); i++ )); do
+			STREAMS+="[${i}:v][${i}:a]"
+		done
+
+		MERGEOPTS=(-crf 0 -qp 0 -preset ultrafast -c:a pcm_f32le \
+			-filter_complex "${STREAMS}"concat=n="${NSTREAMS}":v=1:a=1)
+		;;
+	
+	"muxer" | "m")
+		if [[ ! -r "/tmp/$1.vmr" ]]; then
+			# generate a text file to feed to FFmpeg's concat demuxer,
+			# kinda translating the original file to a format it
+			# understands
+			for FILE in ${ENTRIES[@]}; do
+				# if we got a captioned video file, add the prefix
+				if [[ ! -z "$(grep "$FILE" $1 | awk -F"=" '{ print $2 }')" ]]
+				then
+					echo "file "$(pwd)"/vmerger/"${FILE}"" >> /tmp/$1.vmr
+				else
+					echo "file "${FILE}"" >> /tmp/$1.vmr
+				fi
+			done
+		fi
+
+		MERGEOPTS=(-f concat -i /tmp/$1.vmr -c copy)
+		;;
+
+	*)
+		error "invalid concatenation method! \
+			\nValid ones are: (f)ilter, (m)uxer."
+		;;
 
 # now that we're prepared, actually fire it up
 # and make it merge all the videos we've got
-ffmpeg -v "${LOGLVL}" -threads "${THREADS}" "${FILES[@]}" \
-	-crf 0 -qp 0 -preset ultrafast -c:a pcm_f32le \
-	-filter_complex "${STREAMS}"concat=n="${NSTREAMS}":v=1:a=1 \
-	"${OUTFILE}"
+ffmpeg -v "${LOGLVL}" -threads "${THREADS}" \
+	"${FILES[@]}" "${MERGEOPTS[@]}" "${OUTFILE}"
 	# and finally use last file entry as our output file name
